c08b6f047513c297e8bce1a679db2c6f30db42fe
« Ephely » <ephely1@gmail.com>
2026-01-09 11:01:58 +0100

    Tasks

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -8,7 +8,6 @@ function createClassRoom(numbersOfStudents) {
   for (var i = 0; i < numbersOfStudents; i++) {
     students.push(studentSeat(i + 1));
   }
-
   return students;
 }
 var classRoom = createClassRoom(10);
